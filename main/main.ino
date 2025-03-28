bb29ed5635995f575ff6c5b3eecc146235640cb2
PaulSabatini <paul.sabatini@education.lu>
2025-03-28 04:01:16 +0100

    update main for use with data string

--- main/main.ino
+++ main/main.ino
@@ -14,7 +14,8 @@ int number_of_arrays = 3;
 float* data_arrays[] = {bme_data, gps_data, calibrated_lsm_data};
 size_t data_array_sizes[] = {4, 6, 9};
 int precisions[] = {1, 5, 4}; // This array holds the digits precision needed for each sensor
-
+// Create buffer for data string
+char data_string[MESSAGE_BUFFER_SIZE] = {0};
 
 void setup() {
   // Set I2C pins
@@ -30,10 +31,8 @@ void setup() {
 
   // Start Serial and wait until it opens
   Serial.begin(115200);
-  #ifndef DEBUG_MODE
-    while (!Serial)
-      delay(200);
-  #endif
+  while (!Serial)
+    delay(200);
 
   setup_bme(bme);
   setup_gps(gps);
@@ -44,7 +43,7 @@ void setup() {
   // Need to call setup_sd_card() even if no SD card is connected!
   // Else: SPI has problems and it won't work
   setup_rf95(rf95);
-
+  delay(1000);
 }
 
 int iteration_counter = 0;
@@ -56,7 +55,10 @@ void loop() {
   get_bme_data(bme_data);
   get_gps_data(gps_data);
   get_calibrated_lsm9ds1_data(lsm, calibrated_lsm_data, bias_corrected_magnetic);
-  write_data_to_file(logfile_name, number_of_arrays, data_arrays, data_array_sizes, precisions);
-  send_data_rf95(rf95, iteration_counter, data_arrays, data_array_sizes, number_of_arrays, precisions);
+  create_data_string(data_string, iteration_counter, data_arrays, data_array_sizes, number_of_arrays, precisions);
+  // need to do create_data_string before write_data_to_file!!!
+  write_data_to_file(logfile_name, data_string);
+  send_data_rf95(rf95, data_string);
   iteration_counter++;
+  delay(50);
 }
