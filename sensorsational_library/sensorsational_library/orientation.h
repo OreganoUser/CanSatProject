8e93363960dc0047b8bbfb9098cfe1b307374167
PaulSabatini <paul.sabatini@education.lu>
2025-04-28 09:30:32 +0200

    deleted extra folder

--- sensorsational_library/sensorsational_library/orientation.h
+++
@@ -1,8 +0,0 @@
-#ifndef ORIENTATION_H
-#define ORIENTATION_H
-
-extern float orientation_data[];
-extern int orientation_precisions[];
-void update_mag_heading(int execution_number);
-
-#endif //ORIENTATION_H
